8232e4441ca7e2281696be75c8b967602a10019b
Tantai07 <167741192+Tantai07@users.noreply.github.com>
2024-09-05 21:30:56 +0700

    Change Code Portal

--- Assets/Dungeon/Portal.cs
+++ Assets/Dungeon/Portal.cs
@@ -2,16 +2,38 @@ using UnityEngine;
 
 public class Portal : MonoBehaviour
 {
+    public float detectionRadius = 5.0f;
+    public LayerMask playerLayer;
+    public GameObject TpButton;
     DungeonMaker dungeon;
     private void Start()
     {
         dungeon = FindAnyObjectByType<DungeonMaker>();
+        playerLayer = LayerMask.GetMask("Player");
+        TpButton = transform.Find("Tp_Button").gameObject;
+        TpButton.SetActive(false);
     }
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        IsPlayerInRange();
+    }
+    bool IsPlayerInRange()
+    {
+        Collider2D[] hitColliders = Physics2D.OverlapCircleAll(transform.position, detectionRadius, playerLayer);
+        foreach (Collider2D hitCollider in hitColliders)
         {
-            dungeon.DestroyDungeon();
+            if (hitCollider.CompareTag("Player"))
+            {
+                Debug.Log("Found");
+                TpButton.SetActive(true);
+                return true;
+            }
         }
+        TpButton.SetActive(false);
+        return false;
+    }
+    public void ChangeFloor()
+    {
+        dungeon.DestroyDungeon();
     }
 }
