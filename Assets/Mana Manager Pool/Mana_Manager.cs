0bddf8aef160d9eb9f0d73b9bc4d4801f3b2e635
Tantai07 <167741192+Tantai07@users.noreply.github.com>
2024-09-21 14:25:27 +0700

    Update Mana_Manager.cs

--- Assets/Mana Manager Pool/Mana_Manager.cs
+++ Assets/Mana Manager Pool/Mana_Manager.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class Mana_Manager : MonoBehaviour
 {
     public GameObject Mana;
-    public GameObject[] ManaPool = new GameObject[10];
+    public GameObject[] ManaPool = new GameObject[15];
     void Start()
     {
         for (int i = 0; i < ManaPool.Length; i++)
