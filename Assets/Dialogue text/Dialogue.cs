0a8316a5d9f326152f7e602c062b27c53857132b
Tantai07 <167741192+Tantai07@users.noreply.github.com>
2024-09-24 01:30:24 +0700

    Update Dialogue

--- Assets/Dialogue text/Dialogue.cs
+++ Assets/Dialogue text/Dialogue.cs
@@ -1,17 +1,26 @@
-using System.Collections;
+﻿using System.Collections;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
+[System.Serializable]
+public class RoomDialogue
+{
+    public string roomName;
+    public string[] lines;
+}
+
 public class Dialogue : MonoBehaviour
 {
+    public List<RoomDialogue> rooms = new List<RoomDialogue>();
     public static Dialogue instance;
     public TextMeshProUGUI text;
-    public string[] line1;
-    public string[] line2;
     public float textSpeed;
 
     private int index;
-    private string[] currentline;
+    private string[] currentLine;
+    private Dictionary<string, RoomDialogue> dialogueDictionary = new Dictionary<string, RoomDialogue>();
+
     private void Awake()
     {
         if (instance == null)
@@ -22,58 +31,74 @@ public class Dialogue : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
+        foreach (var room in rooms)
+        {
+            dialogueDictionary[room.roomName] = room;
+        }
     }
+
     void Start()
     {
-        if (DungeonSystem.instance.Level == 1)
-        {
-            currentline = new string[line1.Length];
-        }
-        else if(DungeonSystem.instance.Level == 15)
-        {
-
-        }
-        text.text = string.Empty;
-        StartDialogue();
+        SetDialogue("Shop Room");
     }
+
     void Update()
     {
-        if(Input.GetKeyUp(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.G))
         {
-            if(text.text == currentline[index])
+            if (text.text == currentLine[index])
             {
                 NextLine();
             }
             else
             {
                 StopAllCoroutines();
-                text.text = currentline[index];
+                text.text = currentLine[index];
             }
         }
     }
+
     public void StartDialogue()
     {
         index = 0;
         StartCoroutine(TypeLine());
     }
+
     IEnumerator TypeLine()
     {
-        foreach(char c in currentline[index].ToCharArray())
+        foreach (char c in currentLine[index].ToCharArray())
         {
             text.text += c;
             yield return new WaitForSeconds(textSpeed);
         }
     }
+
     void NextLine()
     {
-        if(index < currentline.Length - 1) 
+        if (index < currentLine.Length - 1)
         {
             index++;
             text.text = string.Empty;
             StartCoroutine(TypeLine());
-        }else
+        }
+        else
         {
             gameObject.SetActive(false);
+        }
+    }
+
+    public void SetDialogue(string roomName)
+    {
+        if (dialogueDictionary.TryGetValue(roomName, out RoomDialogue selectedRoom))
+        {
+            currentLine = selectedRoom.lines;
+            text.text = string.Empty;
+            StartDialogue();
+        }
+        else
+        {
+            Debug.LogWarning("Room type not found.");
         }
     }
 }
